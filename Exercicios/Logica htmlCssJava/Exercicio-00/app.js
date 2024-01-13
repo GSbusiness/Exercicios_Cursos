5656e033caf18b4ba17befdff9411f7227b1a69f
Gabriel Schoepf <gschoepf.business@gmail.com>
2024-01-13 19:24:55 -0300

    excutando primeiro commit

---
+++ Exercicios/Logica htmlCssJava/Exercicio-00/app.js
@@ -0,0 +1,16 @@
+alert('Boas vindas ao nosso site !');
+let nome = lua;
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro, preencha todos os campos');
+let mensagemDeErro = ('Erro, preencha todos os campos');
+alert(mensagemDeErro);
+
+nome = prompt('Qual seu nome ?');
+console.log(nome);
+
+idade = prompt('qual a sua idade ?');
+console.log(idade);
+
+ idade <= 18 ? alert('Pode tirar sua habilitação') : alert('não podee tirar sua habilitação');
